14eaaa5c03f3b4e5053ca8631c2296b1813cf91f
Junhao Fu <jfu@sva.edu>
2018-11-01 13:51:22 -0600

    Add basic movement

---
+++ Assets/Source/Character/Characters.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Characters : MonoBehaviour {
+
+    public Texture CharacterTexture;
+
+    public Characters()
+    {
+        //int = CharacterTexture.height
+        //var vertices: Vector3[] = new Vector3[4];
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Source/Character/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Characters
+{
+
+    //Var
+    public float speed;
+    public GUIText textOutput;
+
+    // Use this for initialization
+    void Start ()
+    {
+     
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Movement();
+    }
+
+    void bindkey()
+    {
+
+    }
+
+    void Movement()
+    {
+        float MoveX = Input.GetAxis("Horizontal");
+        float MoveZ = Input.GetAxis("Vertical");
+
+        float xDeltaPos = MoveX * speed;
+        float zDeltaPos = MoveZ * speed;
+
+        transform.Translate(new Vector3(xDeltaPos, 0, zDeltaPos));
+        textOutput.text = "Horizontal Value Returned: " + xDeltaPos.ToString("F2") + "\nVertical Value Returned: " + zDeltaPos.ToString("F2");
+    }
+}
